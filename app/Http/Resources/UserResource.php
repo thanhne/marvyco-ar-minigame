8aa233588d0558bc12fcb28190262d380c7b1d72
ThanhNee <nbthanh93@gmail.com>
2023-04-12 23:33:26 +0700

    update UserResource and api.php

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -15,6 +15,7 @@ class UserResource extends JsonResource
     public function toArray($request)
     {
         return [
+            'id' => $this->id,
             'name' => $this->name,
             'phone_number' => $this->phone_number,
             'points' => $this->points
